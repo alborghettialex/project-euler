dc1992bbf2403e5a844f43dcc8e7c3c93a0a3973
alborghettialex1998@gmail.com <alborghettialex1998@gmail.com>
2026-05-09 23:10:08 +0200

    Add problem 4 (immediate solution)

---
+++ src/bin/4.rs
@@ -0,0 +1,24 @@
+// # 4
+// A palindromic number reads the same both ways.
+// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
+// Find the largest palindrome made from the product of two $3$-digit numbers.
+
+fn is_palindrome(n: u32) -> bool {
+    let s = n.to_string();
+    s.chars().eq(s.chars().rev())
+}
+
+fn main() {
+    let mut max_pal_found = 0;
+    for n in (1..999).rev() {
+        for m in (1..999).rev() {
+            let prod = n * m;
+            if is_palindrome(n * m) {
+                if prod > max_pal_found {
+                    max_pal_found = prod;
+                }
+            }
+        }
+    }
+    println!("{}", max_pal_found) // 906609
+}
