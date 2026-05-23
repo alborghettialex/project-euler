11fc33551917f130ed71e36b2a2cf9fd0a422cb3
alborghettialex1998@gmail.com <alborghettialex1998@gmail.com>
2026-05-23 13:33:46 +0200

    Add problem 9

---
+++ src/bin/9.rs
@@ -0,0 +1,26 @@
+// # 8
+//
+// A Pythagorean triplet is a set of three natural numbers, a,b,c, for which a^2 + b^2 = c^2.
+// For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2$.
+// There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+// Find the product abc.
+
+fn is_pythagorean(a: u32, b: u32, c: u32) -> bool {
+    a.pow(2) + b.pow(2) == c.pow(2)
+}
+
+fn main() {
+    'outer: for a in 1..=1000 {
+        for b in 1..=1000 {
+            if a + b < 1000 {
+                let c = 1000 - a - b;
+                if is_pythagorean(a, b, c) {
+                    println!("a={}, b={}, c={}, abc={}", a, b, c, a * b * c);
+                    break 'outer;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+}
